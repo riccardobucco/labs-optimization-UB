daed222de3279e985863c3120ab3eea763c45808
Riccardo Bucco <riccardobucco@gmail.com>
2017-10-03 22:07:49 +0200

    Compute and plot the gradient (lab01, 2-dim case)
    The script computes the derivatives of the two dimensional function
    of the 1st laboratory (two dimensional case) with respect to each
    dimension.
    
    A plot of each derivative is created. Both the plots are rotated in
    such a way that it's possible to appreciate their features.

--- lab01/Scripts/two-dimensional-case.py
+++ lab01/Scripts/two-dimensional-case.py
@@ -42,3 +42,41 @@ levels_number = 100
 ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
 # Save the current figure
 plt.savefig('../Images/01-two-dimensional-function-contours.png')
+
+# Compute the derivatives of the 2D function with respect to each dimension
+gradient_X1, gradient_X2 = np.gradient(Y)
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax_gradient_X1 = fig.add_subplot(nrows, ncols, plot_number, projection='3d')
+# Set the labels for the axes
+ax_gradient_X1.set_xlabel('$x_1$')
+ax_gradient_X1.set_ylabel('$x_2$')
+ax_gradient_X1.set_zlabel(r'$\frac{\partial f}{\partial x_1}(x_1, x_2)$')
+# Rotate the axes (azim is the azimuth angle in the x,y plane)
+ax_gradient_X1.view_init(azim=330)
+# Plot a surface with a solid linestyle connecting all the vertices
+ax_gradient_X1.plot_surface(X1, X2, gradient_X1, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
+# Save the current figure
+plt.savefig('../Images/01-two-dimensional-function-gradient-X1.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax_gradient_X2 = fig.add_subplot(nrows, ncols, plot_number, projection='3d')
+# Set the labels for the axes
+ax_gradient_X2.set_xlabel('$x_1$')
+ax_gradient_X2.set_ylabel('$x_2$')
+ax_gradient_X2.set_zlabel(r'$\frac{\partial f}{\partial x_1}(x_1, x_2)$')
+# Rotate the axes (azim is the azimuth angle in the x,y plane)
+ax_gradient_X2.view_init(azim=330)
+# Plot a surface with a solid linestyle connecting all the vertices
+ax_gradient_X2.plot_surface(X1, X2, gradient_X2, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
+plt.savefig('../Images/01-two-dimensional-function-gradient-X2.png')
