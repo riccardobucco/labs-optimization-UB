d177ca59825f489e8c11af8d8bd9bff9ed7d01b6
Riccardo Bucco <riccardobucco@gmail.com>
2017-10-04 16:42:48 +0200

    Plot two lines indicating the abscissa of extrema
    It is useful to have two lines indicating the abscissa of extrema in
    the two dimensional function (1st laboratory). In this way, it easier
    to check whether or not they correspond to the values computed
    analitically.
    
    In this case, the lines y = -0.8165 and y = 0.8165 are plotted: these
    values are the approximate roots of the equation 3x^2 - 2 = 0.

--- lab01/Scripts/one-dimensional-case.py
+++ lab01/Scripts/one-dimensional-case.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 INTERVAL_START = -2
 INTERVAL_STOP = 2
 SAMPLING_INTERVAL = 0.1
+
 # Create a new figure
 fig = plt.figure()
 # Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
@@ -20,3 +21,12 @@ Y = X**3 - 2*X + 2
 ax.plot(X, Y)
 # Save the current figure
 plt.savefig('../Images/01-one-dimensional-function-example.png')
+
+# Add two dashed lines indicating where the (analitically) computed extrema are located
+X = [0.8165, 0.8165]
+Y = [Y[0], Y[-1]] # Y[-1] is the last element of the Y array
+ax.plot(X, Y, color='red', linestyle='dashed')
+X = [-0.8165,-0.8165]
+ax.plot(X, Y, color='red', linestyle='dashed')
+# Save the current figure
+plt.savefig('../Images/01-one-dimensional-function-example-with-extrema')
