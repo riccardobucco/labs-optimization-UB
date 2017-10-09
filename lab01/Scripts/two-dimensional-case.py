44ac85c692cfea4d826fb5e4252bda073310a09d
Riccardo Bucco <riccardobucco@gmail.com>
2017-10-09 21:29:05 +0200

    Add some features to the 2-dim script of 1st lab
    The two dimensional case script of the first laboratory can now create
    some images useful for studying the functions of the laboratory.

--- lab01/Scripts/two-dimensional-case.py
+++ lab01/Scripts/two-dimensional-case.py
@@ -80,3 +80,99 @@ ax_gradient_X2.view_init(azim=330)
 # Plot a surface with a solid linestyle connecting all the vertices
 ax_gradient_X2.plot_surface(X1, X2, gradient_X2, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
 plt.savefig('../Images/02-two-dimensional-function-1-gradient-X2.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number, projection='3d')
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+ax.set_zlabel('$f_A(x_1, x_2)$')
+# Plot a surface with a solid linestyle connecting all the vertices
+Y = - X1**2 - X2**2
+ax.plot_surface(X1, X2, Y, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
+plt.savefig('../Images/02-two-dimensional-function-A-surface.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+# Save the current figure
+plt.savefig('../Images/02-two-dimensional-function-A-contours.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number, projection='3d')
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+ax.set_zlabel('$f_B(x_1, x_2)$')
+# Plot a surface with a solid linestyle connecting all the vertices
+Y = X1**2 - X2**2
+ax.plot_surface(X1, X2, Y, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
+plt.savefig('../Images/02-two-dimensional-function-B-surface.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+# Save the current figure
+plt.savefig('../Images/02-two-dimensional-function-B-contours.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number, projection='3d')
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+ax.set_zlabel('$f_C(x_1, x_2)$')
+# Plot a surface with a solid linestyle connecting all the vertices
+Y = X1**2
+ax.plot_surface(X1, X2, Y, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='inferno')
+plt.savefig('../Images/02-two-dimensional-function-C-surface.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+# Save the current figure
+plt.savefig('../Images/02-two-dimensional-function-C-contours.png')
