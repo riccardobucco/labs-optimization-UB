53141f38217588da3cdc22912e5977eba2892ee0
Riccardo Bucco <riccardobucco@gmail.com>
2017-10-20 11:42:59 +0200

    Add a script for the 1st part of the 2nd report
    The script performs the third experiment of the gradient descent
    methods section of the second report. This experiment focuses on
    different functions which have gradiets pointing in the same direction:
    different values of alpha must be used in order to minimize the
    functions. Another way to solve the problem is to use the normalized
    gradient: in this way you can use the same value of alpha for the
    different functions.

---
+++ lab02/Scripts/01-gradient-descent-3rd-experiment.py
@@ -0,0 +1,173 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+INTERVAL_START_X1 = -5
+INTERVAL_STOP_X1 = 5
+INTERVAL_START_X2 = -5
+INTERVAL_STOP_X2 = 5
+SAMPLING_INTERVAL = 0.01
+ITERATIONS = 100
+ALPHA = 0.01
+STARTING_POINTS = np.array([[4,4], [-3,2], [4,-3]])
+COLORS = np.array(['g', 'r', 'y'])
+
+# Function that computes the value of the specified function in a point x=(x1,x2)
+def myFirstFunction(x):
+    y = x[0]**2 + x[1]**2
+    return y
+
+# Function that computes the value of the specified function in a point x=(x1,x2)
+def mySecondFunction(x):
+    y = 100*x[0]**2 + 100*x[1]**2
+    return y
+
+# Function that computes the gradient (gx1, gx2) of the specified function in a point x=(x1,x2)
+def myFirstFunctionGradient(x):
+    gradient = np.array([2*x[0], 2*x[1]])
+    return gradient
+
+# Function that computes the gradient (gx1, gx2) of the specified function in a point x=(x1,x2)
+def mySecondFunctionGradient(x):
+    gradient = np.array([200*x[0], 200*x[1]])
+    return gradient
+
+# Function that implements the gradient descent method.
+# It requires a starting point, alpha, a function that computes the value of a function in a specified point,
+# a function that computes the gradient of a function in a specified point, the number of iterations to perform
+# It returns an array of points, where each point is computed using the gradient descent method.
+def gradientDescent(x0, alpha, function, function_gradient, iterations):
+    points = [x0]
+    xk = x0
+    gradient_xk = function_gradient(xk)
+    for i in range(0, iterations):
+        xk = xk - alpha*gradient_xk
+        gradient_xk = function_gradient(xk)
+        points.append(xk)
+    return np.array(points)
+
+# Function that implements the normalized gradient descent method.
+# It requires a starting point, alpha, a function that computes the value of a function in a specified point,
+# a function that computes the gradient of a function in a specified point, the number of iterations to perform
+# It returns an array of points, where each point is computed using the normalized gradient descent method.
+def normalizedGradientDescent(x0, alpha, function, function_gradient, iterations):
+    points = [x0]
+    xk = x0
+    gradient_xk = function_gradient(xk)
+    for i in range(0, iterations):
+        xk = xk - alpha*(gradient_xk/np.linalg.norm(gradient_xk))
+        gradient_xk = function_gradient(xk)
+        points.append(xk)
+    return np.array(points)
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+X1 = np.linspace(INTERVAL_START_X1, INTERVAL_STOP_X1, ((INTERVAL_STOP_X1 - INTERVAL_START_X1) / SAMPLING_INTERVAL) + 1)
+X2 = np.linspace(INTERVAL_START_X2, INTERVAL_STOP_X2, ((INTERVAL_STOP_X2 - INTERVAL_START_X2) / SAMPLING_INTERVAL) + 1)
+X1, X2 = np.meshgrid(X1, X2)
+Y = X1**2 + X2**2
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+
+# Apply the gradient method to the function starting at the specified points
+for i in range(0, COLORS.size):
+    x0 = STARTING_POINTS[i]
+    points = gradientDescent(x0, ALPHA, myFirstFunction, myFirstFunctionGradient, ITERATIONS)
+    ax.plot(points[:,0], points[:,1], COLORS[i]+'o')
+
+# Save the current figure
+plt.savefig('../Images/01-gradient-descent-3rd-experiment-first-function.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+X1 = np.linspace(INTERVAL_START_X1, INTERVAL_STOP_X1, ((INTERVAL_STOP_X1 - INTERVAL_START_X1) / SAMPLING_INTERVAL) + 1)
+X2 = np.linspace(INTERVAL_START_X2, INTERVAL_STOP_X2, ((INTERVAL_STOP_X2 - INTERVAL_START_X2) / SAMPLING_INTERVAL) + 1)
+X1, X2 = np.meshgrid(X1, X2)
+Y = X1**2 + X2**2
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+
+# Apply the gradient method to the function starting at the specified points
+for i in range(0, COLORS.size):
+    x0 = STARTING_POINTS[i]
+    points = gradientDescent(x0, ALPHA, mySecondFunction, mySecondFunctionGradient, ITERATIONS)
+    ax.plot(points[:,0], points[:,1], COLORS[i]+'o')
+
+# Save the current figure
+plt.savefig('../Images/01-gradient-descent-3rd-experiment-second-function.png')
+
+# Change the value of alpha (to be used for the normalized gradient descent method)
+ALPHA = 0.1
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+X1 = np.linspace(INTERVAL_START_X1, INTERVAL_STOP_X1, ((INTERVAL_STOP_X1 - INTERVAL_START_X1) / SAMPLING_INTERVAL) + 1)
+X2 = np.linspace(INTERVAL_START_X2, INTERVAL_STOP_X2, ((INTERVAL_STOP_X2 - INTERVAL_START_X2) / SAMPLING_INTERVAL) + 1)
+X1, X2 = np.meshgrid(X1, X2)
+Y = X1**2 + X2**2
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+
+# Apply the gradient method to the function starting at the specified points
+for i in range(0, COLORS.size):
+    x0 = STARTING_POINTS[i]
+    points = normalizedGradientDescent(x0, ALPHA, myFirstFunction, myFirstFunctionGradient, ITERATIONS)
+    ax.plot(points[:,0], points[:,1], COLORS[i]+'o')
+
+# Save the current figure
+plt.savefig('../Images/01-gradient-descent-3rd-experiment-first-function-normalized.png')
+
+# Create a new figure
+fig = plt.figure()
+# Split the figure in 1*1 (nrows*ncols) subaxes and create a new suplot positioned at 1 (plot_number)
+nrows = 1
+ncols = 1
+plot_number = 1
+ax = fig.add_subplot(nrows, ncols, plot_number)
+# Set the labels for the axes
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+# Plot filled contours (up to levels_number automatically-chosen levels)
+X1 = np.linspace(INTERVAL_START_X1, INTERVAL_STOP_X1, ((INTERVAL_STOP_X1 - INTERVAL_START_X1) / SAMPLING_INTERVAL) + 1)
+X2 = np.linspace(INTERVAL_START_X2, INTERVAL_STOP_X2, ((INTERVAL_STOP_X2 - INTERVAL_START_X2) / SAMPLING_INTERVAL) + 1)
+X1, X2 = np.meshgrid(X1, X2)
+Y = X1**2 + X2**2
+levels_number = 100
+ax.contourf(X1, X2, Y, levels_number, cmap='inferno')
+
+# Apply the gradient method to the function starting at the specified points
+for i in range(0, COLORS.size):
+    x0 = STARTING_POINTS[i]
+    points = normalizedGradientDescent(x0, ALPHA, mySecondFunction, mySecondFunctionGradient, ITERATIONS)
+    ax.plot(points[:,0], points[:,1], COLORS[i]+'o')
+
+# Save the current figure
+plt.savefig('../Images/01-gradient-descent-3rd-experiment-second-function-normalized.png')
