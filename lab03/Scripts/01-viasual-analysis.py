a91cbd58015fe4c4fd55135f284300f77d7d5923
Riccardo Bucco <riccardobucco@gmail.com>
2017-11-06 12:56:31 +0100

    Refactor the visual analysis script of 3d lab
    The data regarding the various datasets are stored globally, so that
    they can be used even outside the for loop, for future uses.

--- lab03/Scripts/01-viasual-analysis.py
+++ lab03/Scripts/01-viasual-analysis.py
@@ -12,21 +12,27 @@ def get_dataset(filename):
         y.append(float(instance['y']))
     return np.array(x), np.array(y)
 
+x = {}
+y = {}
+
+# Get the datasets
+for plot_number in range(1,5):
+    filename = '../Data/anscombe-dataset-' + str(plot_number) + '.csv'
+    x[plot_number],y[plot_number] = get_dataset(filename)
+
+# Start the visual analysis of the given datasets
 # Create a new figure
 fig = plt.figure()
 # Split the figure in 2*2 (nrows*ncols) subaxes
 nrows = 2
 ncols = 2
 for plot_number in range(1,5):
-    # Get the dataset
-    filename = '../Data/anscombe-dataset-' + str(plot_number) + '.csv'
-    x,y = get_dataset(filename)
     # Create a new suplot positioned at plot_number
     ax = fig.add_subplot(nrows, ncols, plot_number)
     # Set the limits of the axes
-    ax.set_xlim([min(x)-2,max(x)+2])
-    ax.set_ylim([min(y)-2,max(y)+2])
+    ax.set_xlim([min(x[plot_number])-2,max(x[plot_number])+2])
+    ax.set_ylim([min(y[plot_number])-2,max(y[plot_number])+2])
     # Plot the points of the dataset
-    ax.plot(x,y, 'ro')
+    ax.plot(x[plot_number],y[plot_number], 'bo')
 # Save the current figure
 plt.savefig('../Images/01-visual-analysis.png')
